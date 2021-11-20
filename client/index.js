e3fbb91e1a4bd5d2611746f0279e740fe5c688cb
Robert <robert.koulakes@gmail.com>
2021-11-20 14:22:29 -0800

    Rectifying discrepancies

--- client/index.js
+++ client/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import App from './App';
 
+//https://www.pluralsight.com/guides/how-to-use-geolocation-call-in-reactjs
+//import { useLocation } from 'react-router-dom';
+
+
 ReactDOM.render(<App />, document.getElementById("root"));
